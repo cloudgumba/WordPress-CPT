336031bfc2c79bb45744e8bd8a10add3674e69d9
Manjul <codemadan@gmail.com>
2022-03-04 00:15:04 +0545

    [add] create costum taxonomy

---
+++ custom-taxonomy.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created for CloudGumba Technology.
+ * URL: http://cloudgumba.com
+ * User: codemadan, cloudgumba
+ */
+
+
+/************* Making Custom Taxonomy ***********************/
+add_action( 'init', 'create_notices_taxonomies', 0 );
+function create_notices_taxonomies() {
+    $labels = array(
+        'name'              => _x( 'Category', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Category', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Categories' ),
+        'all_items'         => __( 'All Categories' ),
+        'parent_item'       => __( 'Parent Category' ),
+        'parent_item_colon' => __( 'Parent Category:' ),
+        'edit_item'         => __( 'Edit Category' ),
+        'update_item'       => __( 'Update Category' ),
+        'add_new_item'      => __( 'Add New Category' ),
+        'new_item_name'     => __( 'New Category Name' ),
+        'menu_name'         => __( 'Category' ),
+    );
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'notices_category' ),
+    );
+
+    register_taxonomy( 'notices_category', array( 'notices' ), $args );
+}
